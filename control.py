842665c63b0bed72619934b0b9087cbda522fb6e
BhargavaGowda <bgow529@aucklanduni.ac.nz>
2023-02-07 17:42:17 +1100

    mushed to control

--- control.py
+++ control.py
@@ -19,31 +19,54 @@ pygame.key.set_repeat(100) #holding a key sends continuous KEYDOWN events. Input
 ppi.set_velocity(0,0)
 print("initialise camera")
 camera = ppi.VideoStreamWidget('http://localhost:8080/camera/get')
+INNER_WHEEL = 25
+OUTER_WHEEL = 35
 
+def steer_away_from_green(img: np.ndarray) -> str:
+    """returns a driving command to drive away from the centroid of the green channel of img
+    
+    Args:
+        img: imput image
+        
+    Returns:
+        either 'LEFT' or 'RIGHT'
+    """
+
+    assert img is not None, "no image file found"
+
+    moments = cv2.moments(img[:, :, 1])
+    x_centroid = moments['m10'] / moments['m00']
+    y_centroid = moments['m01'] / moments['m00']
+
+    (height, width, channels) = img.shape
+    assert y_centroid <= height
+    assert x_centroid <= width
+    assert y_centroid >= 0
+    assert y_centroid >= 0
+    
+    if x_centroid < width/2:
+        return "RIGHT"
+
+    return "LEFT"
 
 try:
     while True:
+        image = camera.frame
+        command = steer_away_from_green(image)
+        if command == "LEFT":
+            ppi.set_velocity(INNER_WHEEL, OUTER_WHEEL) 
+        elif command == "RIGHT":
+            ppi.set_velocity(OUTER_WHEEL, INNER_WHEEL) 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    print("right")
-                    ppi.set_velocity(20,20)
-                if event.key == pygame.K_RIGHT:
-                    print("right")
-                    ppi.set_velocity(10,0)
-                if event.key == pygame.K_LEFT:
-                    print("left")
-                    ppi.set_velocity(0,10)
                 if event.key == pygame.K_SPACE:
                     print("stop")                    
                     ppi.set_velocity(0,0)
                     raise KeyboardInterrupt
-            if event.type == pygame.KEYUP:
-                ppi.set_velocity(0,0)
-
-        image = camera.frame
 
         
+
+
         
 
         
