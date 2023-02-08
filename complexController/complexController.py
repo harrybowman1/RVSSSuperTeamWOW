caeee15fb661e34629d9f7495f43bac21c276399
BhargavaGowda <bgow529@aucklanduni.ac.nz>
2023-02-08 18:43:20 +1100

    added comms

--- complexController/complexController.py
+++ complexController/complexController.py
@@ -7,14 +7,46 @@ class Controller:
         self.speed = 30
         self.time = 0
 
+    def steer_away_from_green(img: np.ndarray) -> str:
+        """
+        Markus
+        returns a driving command to drive away from the centroid of the green channel of img
+        
+        Args:
+            img: imput image
+            
+        Returns:
+            either 'LEFT' or 'RIGHT'
+        """
+
+        assert img is not None, "no image file found"
+
+        moments = cv2.moments(img[:, :, 1])
+        x_centroid = moments['m10'] / moments['m00']
+        y_centroid = moments['m01'] / moments['m00']
+
+        (height, width, channels) = img.shape
+        assert y_centroid <= height
+        assert x_centroid <= width
+        assert y_centroid >= 0
+        assert y_centroid >= 0
+        
+        return [x_centroid,y_centroid]
+
     def loop(self,inputImage):
 
         #helper stuff
+        # cv2.imwrite("data/"+str(self.time).zfill(6)+".jpg", inputImage) 
         self.time+=1
         image = cv2.resize(inputImage,(32,32))
 
-        #context stack
+        #context stacks
         locationStack = ["no idea where i am"]
+        generalStack = []
+        #comm stack. occasionally give status updates
+        commStack = []
+        if self.time%20 ==0 and len(commStack)>0:
+            print(commStack.pop())
 
         # vague idea of what road looks like. kinda grey
         road = np.array([100,100,100])
@@ -31,14 +63,25 @@ class Controller:
         if not leftRoadSensor:
             leftMotor+=10
             rightMotor-=20
+            if "comm update" in generalStack: commStack.append("turning right")
 
         if not rightRoadSensor:
             rightMotor+=10
             leftMotor-=20
+            if "comm update" in generalStack: commStack.append("turning right")
 
         if (not leftRoadSensor) and (not rightRoadSensor):
             leftMotor=-10
             rightMotor=10
+            if not ("lost the road" in locationStack):
+                locationStack.append("lost the road")
+            if "comm update" in generalStack: commStack.append("turning right")
 
-        # cv2.imwrite("data/"+str(self.time).zfill(6)+".jpg", inputImage) 
+        # communicate state
+        if "comm update" in generalStack:
+            generalStack.remove("comm update")
+        if self.timer%5==0:
+            generalStack.append("comm update")
+
+
         return [leftMotor,rightMotor]
